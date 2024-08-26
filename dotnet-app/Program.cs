e260f47c97f482eeeca7fb20c37c2dc9c651b0b0
Şefik Can Kanber <sefikcankanber@192.168.1.63>
2024-08-26 22:49:33 +0300

    initial commit

---
+++ dotnet-app/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var options = new ConfigurationOptions
+        {
+            EndPoints = { "haproxy:6379" },
+            ConnectRetry = 5,
+            ConnectTimeout = 5000,
+            KeepAlive = 180, 
+            AbortOnConnectFail = false 
+        };
+
+        var redis = await ConnectionMultiplexer.ConnectAsync(options);
+
+        var db = redis.GetDatabase();
+
+        while (true)
+        {
+            try
+            {
+                await db.StringSetAsync("test", "Hello, Redis!");
+
+                var value = await db.StringGetAsync("test");
+                Console.WriteLine($"Value from Redis: {value}");
+            }
+            catch (RedisConnectionException ex)
+            {
+                Console.WriteLine($"Redis connection error: {ex.Message}");
+            }
+
+            await Task.Delay(2000);
+        }
+    }
+}
